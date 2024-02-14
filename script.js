0fb9782cc7d84a9a9e8bac12811522c8911fa9c7
Ritika Shewade <154889620+ritikashewade@users.noreply.github.com>
2024-02-15 00:59:40 +0530

    Added some styling and fetch data according to name of city

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ async function fetchWeatherData(cityName)
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1906379d73b08b65d0f488584dc8ce57`;
 	const response=await fetch(url);
 	const data = await response.json();
-	console.log(data)
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML = data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+ "g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15) + "°C";
+	console.log(data);
 }
 
 
